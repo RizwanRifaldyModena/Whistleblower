4682fe4cb2272ba3edb5579ea44a1facb036c220
Revan Pratama <revan.pratama@modena.com>
2022-10-10 11:39:53 +0700

    Conf

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.scss'
+
 const Login = () => {
     return (
         <div className='wrap_login'>
             <div className='wrap_form_login'>
                 <img src='./asset/modena-logo.png' className='login_logo' />
-                <div className='form_login'>
-                    <input type={'text'} className={'input_login'} placeholder={'Username'}/>
-                    <input type={'text'} className={'input_login'} placeholder={'Username'}/>
-                </div>
-                <button>Submit</button>
+                <h3 className='center-text'>Whistle Blowing System</h3>
+                <form action="">
+                    <div className='form_login'>
+                        <input type={'text'} className={'input_login'} placeholder={'Username'}/>
+                        <input type={'password'} className={'input_login'} placeholder={'Password'}/>
+                    </div>
+                    <button type='submit'>Submit</button>
+                </form>
             </div>
         </div>
     )
